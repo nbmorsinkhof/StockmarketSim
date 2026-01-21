8e52762a06993eaf2e8d5ebcc5d45da8156592a6
Niek <Niek.m@cobotx.nl>
2026-01-21 21:03:48 +0100

    build_installer + app_icon

--- build_installer.bat
+++ build_installer.bat
@@ -1,2 +1,14 @@
+@echo off
+setlocal
+
+REM 1) Rebuild the EXE (PyInstaller)
+pyinstaller --noconfirm --clean StockmarketSim.spec
+if errorlevel 1 exit /b 1
+
+REM 2) Rebuild the installer (Inno Setup)
 set "ISCC=C:\PROGRA~2\INNOSE~1\ISCC.exe"
 "%ISCC%" "installer.iss"
+if errorlevel 1 exit /b 1
+
+echo Done.
+endlocal
